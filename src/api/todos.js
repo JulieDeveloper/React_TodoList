40400c115c7bcf57d3a0e2e0f8b5d542ed6195ea
Julie Chou <chouyuan424@gmail.com>
2023-02-05 16:43:35 -0500

    feat: add todo CRUD function

---
+++ src/api/todos.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001';
+export const getTodos = () => {};
+export const createTodo = () => {};
+export const patchTodo = () => {};
+export const deleteTodo = () => {};
