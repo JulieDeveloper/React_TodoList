c78161a7d2d0f1d2de318949c79ee45946782f2c
Julie Chou <chouyuan424@gmail.com>
2023-02-05 16:46:51 -0500

    feat: implement get todos

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 const baseUrl = 'http://localhost:3001';
-export const getTodos = () => {};
+export const getTodos = async () => {
+  try {
+    const res = await axios.get(`${baseUrl}/todos`);
+    return res.data;
+  } catch (error) {
+    console.error('[Get Todos failed]:', error);
+  }
+};
 export const createTodo = () => {};
 export const patchTodo = () => {};
 export const deleteTodo = () => {};
