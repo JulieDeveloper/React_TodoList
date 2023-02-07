ecb243b0caa23fbd4450bc3b5dc0698371c7323b
Julie Chou <chouyuan424@gmail.com>
2023-02-07 09:24:09 -0500

    feat: add auth function to context - 1.setup provider

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,11 +1,26 @@
-import { createContext } from 'react';
-
+import { createContext, useState } from 'react';
+import { login, register } from '../api/auth';
+import * as jwt from 'jsonwebtoken';
 const defaultAuthContext = {
   isAuthenticated: false,
   currentMember: null,
   register: null,
   login: null,
   logout: null,
+};
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [payload, setPayload] = useState(null);
+
+  return (
+    <AuthProvider
+      value={{
+        isAuthenticated,
+        currentMember: payload,
+      }}
+    ></AuthProvider>
+  );
 };
 
 const AuthContext = createContext(defaultAuthContext);
